70284e066a17df2c7e8e2ae06393e999da9b4fd1
tobias <tobias.schaber@codecentric.de>
2015-06-21 13:16:11 +0200

    adjusted preinstallation scripts

--- post-install.sh
+++ post-install.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
+# this script installs puppet, downloads the theforeman puppet module and installs it into
+# puppet.
+
+
 cd $HOME
 sudo apt-get install --yes openssh-server git
-mkdir -p git
-cd git
 
-if [ ! -d "$HOME/git"  ]; then
-	git clone https://tobias_schaber@bitbucket.org/tobias_schaber/theforeman.git
-fi
+# clone the foreman installer puppet module from git
+git clone https://bitbucket.org/tobias_schaber/theforeman.git
 
+# ATTENTION ! REACTIVATE FOR BAREMETAL USAGE!!!
 #sudo cp $HOME/git/foreman-poc/files/System/interfaces /etc/network/
+
 
 wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
 sudo apt-get update	
 sudo apt-get install --yes puppet
 
+
+# ATTENTION! This is deactivated because its the oldstyle art of adding cloudbox to puppet.
+# 			 we want the new style
 #sudo cp $HOME/git/foreman-poc/files/System/puppet.conf /etc/puppet/
 #sudo service puppet restart
+
+# install the stdlib puppet module
 sudo puppet module install --force puppetlabs-stdlib
 
-rm puppetlabs-release-trusty.deb
-
-
+# copy the downloaded module from git into the puppet modules
 sudo cp -r theforeman /etc/puppet/modules
 
-sudo reboot
+# cleanup
+rm puppetlabs-release-trusty.deb
+
+# ATTENTION! I dont think that a reboot is still required!
+#sudo reboot
+
+
+
+# CONTINUE WITH run-puppet.sh
--- run-puppet.sh
+++ run-puppet.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-sudo puppet module install --force puppetlabs-stdlib
-
-sudo puppet apply --debug /home/server/git/foreman-poc/manifests/server.pp
-
-
-# https://bitbucket.org/tobias_schaber/theforeman/raw/401cf9a25af4c1aca28b72a35ea2bfdf85cd4b75/post-install.sh
-
-
----> Die Gruppe "bind" existiert nicht. Wo wurde sie denn bisher angelegt?
+sudo puppet apply --debug /home/server/theforeman/manifests/main.pp
