0090ae12e20433db7e739e92b141a1c3c4f95d9d
tobias <tobias.schaber@codecentric.de>
2015-06-26 13:22:43 +0200

    added info

--- manifests/installplugins/discoveryplugin.pp
+++ manifests/installplugins/discoveryplugin.pp
@@ -6,11 +6,17 @@ class theforeman::installplugins::discoveryplugin {
 
 	## INSTALLATION SEQUENCE DEFINITION ##
 	
+	Notify["info"] ->
 	Exec['foreman-installer-discovery-plugin']
 	
 	# required??? : apt-get install ruby-smart-proxy-discovery
 
 	## PROCEDURE DEFINITION ##
+	
+	notify { "info": 
+	
+	}
+
 	
 	exec { 'foreman-installer-discovery-plugin':
 		command	=> "foreman-installer --enable-foreman-plugin-discovery --foreman-plugin-discovery-install-images=true",
