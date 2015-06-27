40c292e5fb78df3006498855eb1da3f6df837e6a
tobias <tobias.schaber@codecentric.de>
2015-06-27 14:17:57 +0200

    added os ubuntu 14.04

--- manifests/configuration/operatingsystems.pp
+++ manifests/configuration/operatingsystems.pp
@@ -4,6 +4,7 @@ class theforeman::configuration::operatingsystems {
 
 	## INSTALLATION SEQUENCE DEFINITION ##
 	
+	Exec['hammer-create-ubuntu-trusty'] ->
 	Exec['hammer-set-template-preseed-default'] ->
 	Exec['hammer-set-template-preseed-finish'] ->
 	Exec['hammer-set-template-preseed-pxelinux']
@@ -11,6 +12,12 @@ class theforeman::configuration::operatingsystems {
 	
 	## PROCEDURE DEFINITION ##
 	
+	
+	exec { 'hammer-create-ubuntu-trusty':
+		environment => ["HOME=/home/server"],
+		path 	=> ['/usr/sbin/', '/bin/', '/sbin/', '/usr/bin'],
+		command => "",
+	}
 
 	# update the domain: enter the dns entry id. us OS-id 1 because there is only one OS at the beginning
 	exec { 'hammer-set-template-preseed-default':
@@ -21,16 +28,6 @@ class theforeman::configuration::operatingsystems {
 	
 	# update the domain: enter the dns entry id. us OS-id 1 because there is only one OS at the beginning
 	exec { 'hammer-set-template-preseed-finish':
-		environment => [
-			"HOME=/home/server",
-		],
-		path 	=> ['/usr/sbin/', '/bin/', '/sbin/', '/usr/bin'],
-		command => "hammer template update --id $(hammer template list --search \"Preseed default finish\" | /bin/grep \"Preseed default finish\" | /usr/bin/cut -d' ' -f1) --operatingsystem-ids 1",
-	}
-	
-	
-	
-		exec { 'hammer-set-template-preseed-finish':
 		command => "echo subnet created",
 		onlyif 	=> "hammer template update --id $(hammer template list --search \"Preseed default finish\" | /bin/grep \"Preseed default finish\" | /usr/bin/cut -d' ' -f1) --operatingsystem-ids 1",
 		path 	=> ['/usr/sbin/', '/bin/', '/sbin/', '/usr/bin'],
@@ -38,17 +35,7 @@ class theforeman::configuration::operatingsystems {
 			"HOME=/home/server",
 		],
 		timeout	=> 1000,
-		}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
 	
 	
 	
