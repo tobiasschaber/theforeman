45d5a280e89632e3c554e3c0faf311bd46f52088
tobias <tobias.schaber@codecentric.de>
2015-08-28 10:04:09 +0200

    added pxelinux global default template replacement

--- manifests/configuration/discovery.pp
+++ manifests/configuration/discovery.pp
@@ -4,6 +4,8 @@ class theforeman::configuration::discovery {
 
 	Exec['restart-service-proxy'] ->
 	Exec['hammer-refresh-proxy-features'] ->
+	File['/tmp/pxelinux_global_default_template'] ->
+	Exec['hammer-update-pxe-global-default-template'] ->
 	Notify['finished-installation']
 
 	
@@ -16,8 +18,23 @@ class theforeman::configuration::discovery {
 		environment => ["HOME=/home/server"],
 		path 	=> ['/usr/sbin/', '/bin/', '/sbin/', '/usr/bin'],
 		command => "echo refreshing hammer proxy features",
-		onlyif => "hammer proxy refresh-features --id 1",
+		onlyif  => "hammer proxy refresh-features --id 1",
 	}
+	
+	
+	file {'/tmp/pxelinux_global_default_template':
+		ensure	=> present,
+		source 	=> 'puppet:///modules/theforeman/configuration/pxelinux_global_default_template.txt',
+	}		
+
+	exec { 'hammer-update-pxe-global-default-template':
+		environment => ["HOME=/home/server"],
+		path 	=> ['/usr/sbin/', '/bin/', '/sbin/', '/usr/bin'],
+		command => "echo updated pxe global default template",
+		onlyif  => "hammer template update --id $(hammer template list --search \"PXELinux global default\" | grep \"PXELinux global default\" | cut -c 1,1) --file /tmp/pxelinux_global_default_template",
+		require => File['/tmp/pxelinux_global_default_template'],
+	}
+	
 
 	notify { 'finished-installation':
 	
